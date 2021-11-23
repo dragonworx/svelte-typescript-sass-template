cdecf86298b4a5b90f27526053e38451588727a0
dragonworx <dragonworxau@yahoo.com.au>
2021-11-24 10:02:43 +1100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	@import "theme";
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: $color;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		border: 1px solid $color;
+		border-radius: $radius;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
